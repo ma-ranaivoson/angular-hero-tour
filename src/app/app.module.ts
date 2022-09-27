681ebeba5b43af7631d449dbee5a53ca4917ac70
ma-ranaivoson <rv.nirmalala@gmail.com>
2022-09-27 11:42:23 +0300

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RoutingModule } from './routing.module';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, RoutingModule],
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
+    DashboardComponent
   ],
   bootstrap: [AppComponent],
 })
